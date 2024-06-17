c3b6e8f86728900ceb37b14f0e1c64ec3346e981
hafidzmulia-its <5002221022@student.its.ac.id>
2024-06-17 19:10:55 +0700

    Catalog updated by haf, todo : catalog responsive and side bar

--- js/script.js
+++ js/script.js
@@ -14,3 +14,24 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+
+// Klik di filter untuk menampilkan atau menghilangkan filter
+var filterIcon = document.querySelector('.catalog-name .fa-filter');
+var dropdownMenu = document.querySelector('.dropdown-menu');
+
+filterIcon.addEventListener('click', function(event) {
+  event.stopPropagation(); // Prevent the document click event from firing
+  if (dropdownMenu.style.display === 'block') {
+    dropdownMenu.style.display = 'none';
+  } else {
+    dropdownMenu.style.display = 'block';
+  }
+});
+
+// Klik di mana saja untuk menghilangkan filter
+document.addEventListener('click', function() {
+  if (dropdownMenu.style.display === 'block') {
+    dropdownMenu.style.display = 'none';
+  }
+});
