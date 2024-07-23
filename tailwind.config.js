13e93e62390bde9f24c82c31ebd80efc7ac25a95
Davit-mtx <firmansahdavit2@gmail.com>
2024-07-23 16:23:44 +0700

    update catalog

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      
+      backgroundColor: {
+        'coklat': 'C6A969',
+        'custom-green-100': '#7FB029', // 100% opacity
+        'custom-green-30': 'rgba(127, 176, 41, 0.3)', // 30% opacity
+      },
+      textColor: {
+        'custom-green-100': '#7FB029', // 100% opacity
+        'custom-green-30': 'rgba(127, 176, 41, 0.3)', // 30% opacity
+      },
     },
   },
   plugins: [],
